810278baa349baceeccf0436c8cb1bb05bab3521
Rico Wilcke <aperolium@gmail.com>
2019-01-01 18:11:12 +0100

    dockerized tests

--- maven_runner.sh
+++ maven_runner.sh
@@ -3,13 +3,3 @@
 echo "START: Running tests..."
 
 mvn test -o -B
-
-if [[ $? -ne 0 ]] ; then
-  echo "FINISH: There are test failures, failing build..."
-  echo "Exiting with code 1."
-  exit 1
-else
-  echo "FINISH: All tests passed!"
-  echo "Exiting with code 0."
-  exit 0
-fi
